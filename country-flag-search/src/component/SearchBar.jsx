4727451e51dfbb15b93cbf603cfcc1e3d5677aac
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-12-04 20:56:03 +0530

    final update

--- country-flag-search/src/component/SearchBar.jsx
+++ country-flag-search/src/component/SearchBar.jsx
@@ -23,6 +23,7 @@ const SearchBar = () => {
                 const res= await axios.get(`https://restcountries.com/v3.1/currency/${currency}`)
                 // console.log(res)
                 setCountries(res.data)
+               
 
             }catch(err){
                 console.error(err)
@@ -31,6 +32,7 @@ const SearchBar = () => {
         }
         if(deCurrency.length>0){
             SearchCountry(deCurrency)
+            
          }
         
     },[deCurrency])
@@ -38,12 +40,12 @@ const SearchBar = () => {
    
   
   return (
-    <Box maxW="60%" m={"50px auto"}>
-    <InputGroup>
+    <Box maxW="60%" m={"50px auto"} >
+    <InputGroup >
     <InputLeftElement pointerEvents='none'>
     <SearchIcon/>
     </InputLeftElement>
-    <Input type='text' value={currency} onChange={(e)=>setCurrency(e.target.value)} placeholder='Search By currency INR, EUR' />
+    <Input  type='text' value={currency} onChange={(e)=>setCurrency(e.target.value)} placeholder='Search By currency INR, EUR' />
   </InputGroup>
   <Grid justifyContent={"center"} templateColumns={{base:'1fr',sm:'repeat(2,1fr)'}}gap={6} mt="50px">
   {
