8c6dab71ee1042ebf54cc834f22977a456f51d20
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-12-05 09:58:05 +0530

    loading

--- country-flag-search/src/component/SearchBar.jsx
+++ country-flag-search/src/component/SearchBar.jsx
@@ -5,28 +5,34 @@ import {Input,InputGroup,InputLeftElement,Card, CardBody,
 import {SearchIcon} from "@chakra-ui/icons"
 import axios from "axios"
 import useDebounce from '../Custom/Debounce'
+import Loading from './Loading'
 
 
 const SearchBar = () => {
 
     const [currency,setCurrency]=useState("")
     const [countries,setCountries]=useState([])
+    const [loading,setLoading]=useState(false)
 
-    const deCurrency=useDebounce(currency,500)
+    const deCurrency=useDebounce(currency,1000)
    
     useEffect(()=>{
 
         const SearchCountry=async(currency)=>{
+
+          setLoading(true)
 
             try{
 
                 const res= await axios.get(`https://restcountries.com/v3.1/currency/${currency}`)
                 // console.log(res)
                 setCountries(res.data)
+                setLoading(false)
                
 
             }catch(err){
                 console.error(err)
+                setLoading(false)
             }
 
         }
@@ -48,6 +54,8 @@ const SearchBar = () => {
     <Input  type='text' value={currency} onChange={(e)=>setCurrency(e.target.value)} placeholder='Search By currency INR, EUR' />
   </InputGroup>
   <Grid justifyContent={"center"} templateColumns={{base:'1fr',sm:'repeat(2,1fr)'}}gap={6} mt="50px">
+
+  {loading && <Loading/>}
   {
     countries &&countries.map(ele=>
 
